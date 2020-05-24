8a9b2ea722cdd4c2bd30144d94605d9840e7c001
Finn Woelm <public@finnwoelm.com>
2020-05-24 11:31:16 +0200

    Try title-casing content-type with multiValueHeaders

---
+++ functions/test6.js
@@ -0,0 +1,13 @@
+exports.handler = function(event, context, callback) {
+  const response = {
+    body: '{"query":{"testqs":"1","works":"true","id":"12345"}}',
+    isBase64Encoded: false,
+    statusCode: 200,
+    multiValueHeaders: {
+      'Content-Type': [ 'application/json; charset=utf-8' ],
+      'content-length': [ 52 ]
+    },
+    level: 'info'
+  }
+  callback(null, response)
+}
