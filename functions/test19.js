1fa068c6bc5c1f5bce9bb3b2874b3b5a93ad297e
Finn Woelm <public@finnwoelm.com>
2020-06-01 19:43:30 +0200

    Test setting cookies
    Try setting multiple cookies.

---
+++ functions/test19.js
@@ -0,0 +1,14 @@
+exports.handler = function(event, context, callback) {
+  const response = {
+    body: '{"query":{"testqs":"1","works":"true","id":"12345"}}',
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'custom-header': 'mycustomheader',
+      'set-cookie': 'mycookie=abc; yourcookie=def'
+    },
+    level: 'info'
+  }
+  callback(null, response)
+}
