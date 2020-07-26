1365b810f81537f76fd4a3bb9aa13efe643ebd64
Finn Woelm <public@finnwoelm.com>
2020-07-26 10:15:41 +0200

    Use readline to update console.log
    As described in https://stackoverflow.com/a/34570694/6451879

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
+var readline = require('readline');
+
 setTimeout(() => log(0), 0000)
 setTimeout(() => log(1), 1000)
 setTimeout(() => log(2), 2000)
 setTimeout(() => log(3), 3000)
 
 function log(number) {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
+  readline.clearLine(process.stdout)
+  readline.cursorTo(process.stdout, 0)
   process.stdout.write("Hello, World" + number);
 }
